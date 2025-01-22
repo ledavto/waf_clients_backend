6148011293de4981c2662c1554071d43aa9f3534
ledavto <ser_ega@ukr.net>
2025-01-23 00:12:03 +0200

    Update bookControllers.js

--- controllers/bookControllers.js
+++ controllers/bookControllers.js
@@ -7,17 +7,8 @@ const createBook = ctrlWrapper(async (req, res) => {
   res.status(201).json(result);
 });
 
-// const fetchUsers = ctrlWrapper(async (req, res) => {
-//   // const { id: ownerFarm } = req.params;
-
-//   const listUsers = await User.find({});
-
-//   res.status(201).json(listUsers);
-// });
-
 const fetchBookByUserId = ctrlWrapper(async (req, res) => {
   const { id } = req.params;
-  console.log(id);
 
   const user = await Book.find({ clientId: id });
 
@@ -35,14 +26,14 @@ const deleteBook = ctrlWrapper(async (req, res) => {
 const updateBook = ctrlWrapper(async (req, res) => {
   const { id } = req.params;
 
-  const user = await User.findByIdAndUpdate(id, name);
+  console.log(req.body);
+  const book = await Book.findByIdAndUpdate(id, req.body);
 
-  res.status(201).json(user);
+  res.status(201).json(book);
 });
 
 module.exports = {
   createBook,
-  // cancelBook,
   fetchBookByUserId,
   deleteBook,
   updateBook,
